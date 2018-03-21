2af0579b8a689b7cf430c2ace273b3e81913908c
Shinkay <shinkayb@gmail.com>
2018-03-21 18:01:45 -0400

    done with js and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,159 +4,135 @@
 
     $("#start").on('click', function(){
             // console.log("You Clicked Me!");
-            // $("#subwrapper").remove();
-            for(var i=0; i<questions.length; i++){
-                $('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
-                for(var j=0; j<questions[i].answers.length; j++){
-                        $('#subwrapper').append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
-                }
-            }
+            game.start();
+            
             
     })
 
 var questions = [{
     question: "Name the world's biggest island?",
     answers: ["Great Britain", "Greenland", "Australia", "Singapore", "New Zealand"],
-    correctAnswer: "Greenland",
-    // image: "assets/images/greenland.gif"
+    correctAnswer: "Greenland"
 }, {
     question: "How many teams are there in the American National Football League?",
     answers: ["10", "15", "22", "28", "32"],
-    correctAnswer: "32",
-    // image: "assets/images/football.gif"
-
+    correctAnswer: "32"
 }, {
     question: "What is the capital city of Spain?",
     answers: ["Madrid", "Barcelona", "Valencia", "Cordoba", "Seville"],
-    correctAnswer: "Madrid",
-    // image: "assets/images/madrid.gif"
+    correctAnswer: "Madrid"
 }, {
 
 }, {
     question: "When did the Cold War end?",
     answers: ["1959", "1999", "1979", "1989", "1969"],
-    correctAnswer: "1989",
-    // image: "assets/images/coldwar.gif"
+    correctAnswer: "1989"
 }, {
     question: "What color is Absynthe beverage?",
     answers: ["Red", "Green", "Orange", "Purple", "White"],
-    correctAnswer: "Green",
-    // image: "assets/images/absynthe.gif"
+    correctAnswer: "Green"
 }, {
     question: "Who played Neo in The Matrix?",
     answers: ["Ian Bliss", "Harry Lennix", "Laurence Fishburne", "Keanu Reeves", "Clayton Watson"],
-    correctAnswer: "Keanu Reeves",
-    // image: "assets/images/neo.gif"
+    correctAnswer: "Keanu Reeves"
 }, {
     question: "What is the diameter of Earth?",
     answers: ["8000 miles", "10,000 miles", "5,000 miles", "2,000 miles", "earth isn't round"],
-    correctAnswer: "8000 miles",
-    // image: "assets/images/earth.gif"
+    correctAnswer: "8000 miles"
 }];
 
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 30,
+    countdown: function () {
+        game.counter--;
+        $("#counter").html(game.counter);
+        if (game.counter <= 0) {
+            console.log("time is up!");
+            game.done();
+        }
+    },
+    start: function () {
+        timer = setInterval (game.countdown,1000);
+        $("#subwrapper").prepend('<h2>Time Remaining: <span id="counter">30</span> Seconds</h2>');
+        $("#start").remove();
+        for (var i = 0; i < questions.length; i++) {
+            $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $('#subwrapper').append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            }
+        }
+    },
+    done: function (){
+        $.each($('input[name="question-0]":checked'), function(){
+            if($(this).val()==questions[0].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-0]":checked'), function(){
+            if($(this).val()==questions[0].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function(){
+            if($(this).val()==questions[1].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-2]":checked'), function(){
+            if($(this).val()==questions[2].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-3]":checked'), function(){
+            if($(this).val()==questions[3].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-4]":checked'), function(){
+            if($(this).val()==questions[4].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-5]":checked'), function(){
+            if($(this).val()==questions[5].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-6]":checked'), function(){
+            if($(this).val()==questions[6].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
 
+        this.result();
+        },
 
-//     var seconds = 30;
-//     var triviaQs = trivia.questions;
-//     var counter = 0;
-//     var gameOverMessage = "Game Over!";
-//     var timeUpMessage = "Time's Up!";
+        result: function(){
+            clearInterval(timer);
+            $('#subwrapper h2').remove();
+            $('#subwrapper').html("<h2>All done! </h2>");
+            $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
+            $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+            $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
 
-//     $(".start-game").on("click", function () {
-//         $('.main-container').show();
-//         $(this).hide();
-//         countdown();
+        }
 
-//     });
-
-//     function decrement() {
-//         seconds--;
-//         var timeRemaining = ('<h2>');
-//         $('#time-remaining').html(timeRemaining + "Seconds Remaining: " + seconds);
-//         if (seconds === 0) {
-//             stop();
-//             $('.message').html(timeUpMessage);
-//             result();
-//         }
-//     };
-
-//     function countdown() {
-//         counter = setInterval(decrement, 1000);
-//     };
-
-//     function stop() {
-//         clearInterval(counter);
-//     };
-
-//     function formTemplate(data) {
-//         var questionString = " <form class='question'> " + data.question + "<br>";
-//         var possibles = data.possibles;
-//         for (var i = 0; i < possibles.length; i++) {
-//             var possibleAnswers = possibles[i];
-//             console.log(possibleAnswers);
-//             questionString = questionString + " <input type='radio' name= '" + data.id + "' value= " + i + " > " + possibleAnswers;
-//         }
-//         return questionString + "</form>";
-//     }
-
-//     function buildQuestions() {
-//         var questionHTML = ''
-//         for (var i = 0; i < triviaQs.length; i++) {
-//             questionHTML = questionHTML + formTemplate(triviaQs[i]);
-//         }
-//         $('#questions-container').append(questionHTML);
-//     };
-
-//     buildQuestions();
-
-//     function isCorrect(data) {
-//         var answers = $(data.id);
-//         var correct = answers.eq(data.answer);
-//         var isChecked = correct.is(':checked');
-//         return isChecked;
-//     };
-
-//     function checkAnswered(data) {
-//         var anyAnswered = false;
-//         var answers = $(data.id);
-//         for (var i = 0; i < answers.length; i++) {
-//             if (answers[i].checked) {
-//                 anyAnswered = true;
-//             }
-//         }
-//         return anyAnswered;
-
-//     };
-
-//     function result() {
-//         var resultsHTML = '';
-//         var guessedAnswers = [];
-//         var correct = 0;
-//         var incorrect = 0;
-//         var unAnswered = 0
-
-//         for (var i = 0; i < triviaQs.length; i++) {
-//             if (isCorrect(triviaQs[i])) {
-//                 correct++;
-//             } else if (checkAnswered(triviaQs[i])) {
-//                 incorrect++;
-//             } else {
-//                 unAnswered++;
-//             }
-//         }
-
-//         $('.results').html('correct: ' + correct + "<br>" + 'incorrect: ' + incorrect + "<br>" + 'unanswered: ' + unAnswered);
-//     };
-
-//     $('.done-button').on('click', function () {
-//         result();
-//         stop();
-//         $("#messageDiv").html(gameOverMessage);
-//     });
-
-
-// });
-
-
-// //PSEUDO CODE
-// // for some reason the questions wouldnt show on the main game page
+    };
